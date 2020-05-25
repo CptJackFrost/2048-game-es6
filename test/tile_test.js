8d7f1384eb10d086ded69b33a9195611ccf13bb5
CptJackFrost <cptjackfrost74@gmail.com>
2020-05-25 23:11:39 +0500

    tile and tile test changed - no drawing there now

--- test/tile_test.js
+++ test/tile_test.js
@@ -1,12 +1,11 @@
-'use strict';
-import {Tile} from '../src/Tile.js';
+import {Tile} from '../src/Tile.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const field = document.querySelector('#playfield');
-    const tile = new Tile(2, {x: 1, y: 1});
-    const tile2 = new Tile(4, {x: 1, y: 2});
+    const tile = new Tile(2, {x: 1, y: 1})
+    const tile2 = new Tile(4, {x: 1, y: 2})
 
-
-    field.appendChild(tile.drawTile());
-    field.appendChild(tile2.drawTile());
+    console.log(tile.x)
+    console.log(tile.value)
+    console.log(tile2.y)
+    console.log(tile2.value)
 })
